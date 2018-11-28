918c79e8d6f1f8b4359c9a3fdb63b27c9f01fe27
Carlos <carlos.cm@telefonica.net>
2018-11-29 00:46:58 +0100

    About
    - About done
    - Added index
    - languaje navigation

--- app/js/someFile.js
+++ app/js/someFile.js
@@ -1,5 +1,21 @@
 function someFunction() {
   console.log('some function');
+
+  $(function () {
+
+
+      //añadiendo jquery e easing
+      require('jquery');
+      require('aos');
+
+      AOS.init({
+       duration: 1200,
+  easing: 'ease-in-out-back'
+      });
+
+
+  });
+
 }
 
 export default someFunction;
