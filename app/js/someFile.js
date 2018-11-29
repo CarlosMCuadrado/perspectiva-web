9d75a49792ef3e9bb69bc97d6354fb580babbb9b
Carlos <carlos.cm@telefonica.net>
2018-11-29 23:14:21 +0100

    - Home-2
    - AOS animation
    - 3D effect (box class)
    - Better indent

--- app/js/someFile.js
+++ app/js/someFile.js
@@ -1,20 +1,20 @@
 function someFunction() {
-  console.log('some function');
+    console.log('some function');
 
-  $(function () {
-
-
-      //añadiendo jquery e easing
-      require('jquery');
-      require('aos');
-
-      AOS.init({
-       duration: 1200,
-  easing: 'ease-in-out-back'
-      });
+    $(function () {
 
 
-  });
+        //añadiendo jquery e easing
+        require('jquery');
+        require('aos');
+
+        AOS.init({
+            duration: 1200,
+            easing: 'ease-in-out-back'
+        });
+
+
+    });
 
 }
 
